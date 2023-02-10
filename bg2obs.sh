fb151cdde9cec7f9a2ca593e2039102579279059
Xtremefaith <nick@divtruth.com>
2023-02-09 23:37:11 -0700

    Improved formatting for readability

--- bg2obs.sh
+++ bg2obs.sh
@@ -16,57 +16,60 @@
 # make sure to honour the copyright restrictions.
 #----------------------------------------------------------------------------------
 
-# OPTIONS
+# Source the config file
 source config.sh
- # Cycling through the book counter, setting which book and it's maxchapter
-  for ((book_counter=0; book_counter <= book_counter_max; book_counter++))
+
+# Loop through each book in the book array
+for ((book_counter=0; book_counter <= book_counter_max; book_counter++))
+do
+  # Get the current book name, max chapter and abbreviation
+  book=${bookarray[$book_counter]}
+  maxchapter=${lengtharray[$book_counter]}
+  abbreviation=${abbarray[$book_counter]}
+
+  # Loop through each chapter of the current book
+  for ((chapter=1; chapter <= maxchapter; chapter++))
   do
+    # Calculate the previous and next chapter
+    prev_chapter=$((chapter-1))
+    next_chapter=$((chapter+1))
 
-    book=${bookarray[$book_counter]}
-    maxchapter=${lengtharray[$book_counter]}
-    abbreviation=${abbarray[$book_counter]}
+    # Set the prefix of the filename
+    export_prefix="${book} "
 
-    for ((chapter=1; chapter <= maxchapter; chapter++))
-    do
+    # Set the chapter number for the filename
+    export_number=${chapter}
+    filename=${export_prefix}$export_number
 
-((prev_chapter=chapter-1)) # Counting the previous and next chapter for navigation
-((next_chapter=chapter+1))
+    # Calculate the filenames for the previous and next chapters
+    prev_file=${export_prefix}$prev_chapter
+    next_file=${export_prefix}$next_chapter
 
-# Exporting
-  export_prefix="${book} " # Setting the first half of the filename
+    # Calculate the navigation links
+    if [ ${maxchapter} -eq 1 ]; then
+      # For a book with only one chapter
+      navigation="[[${book}]]"
+    elif [ ${chapter} -eq ${maxchapter} ]; then
+      # For the last chapter of the book
+      navigation="[[${prev_file}|← ${book} ${prev_chapter}]] | [[${book}]]"
+    elif [ ${chapter} -eq 1 ]; then
+      # For the first chapter of the book
+      navigation="[[${book}]] | [[${next_file}|${book} ${next_chapter} →]]"
+    else
+      # For all other chapters
+      navigation="[[${prev_file}|← ${book} ${prev_chapter}]] | [[${book}]] | [[${next_file}|${book} ${next_chapter} →]]"
+    fi
 
-  export_number=${chapter}
-
-filename=${export_prefix}$export_number # Setting the filename
-
-
-  prev_file=${export_prefix}$prev_chapter # Naming previous and next files
-  next_file=${export_prefix}$next_chapter
-
-  # Formatting Navigation and omitting links that aren't necessary
-  if [ ${maxchapter} -eq 1 ]; then
-    # For a book that only has one chapter
-    navigation="[[${book}]]"
-  elif [ ${chapter} -eq ${maxchapter} ]; then
-    # If this is the last chapter of the book
-    navigation="[[${prev_file}|← ${book} ${prev_chapter}]] | [[${book}]]"
-  elif [ ${chapter} -eq 1 ]; then
-    # If this is the first chapter of the book
-    navigation="[[${book}]] | [[${next_file}|${book} ${next_chapter} →]]"
-  else
-    # Navigation for everything else
-    navigation="[[${prev_file}|← ${book} ${prev_chapter}]] | [[${book}]] | [[${next_file}|${book} ${next_chapter} →]]"
-  fi
-
-  if ${boldwords} -eq "true" && ${headers} -eq "false"; then
-    text=$(ruby BibleGateway-to-Markdown/bg2md.rb -e -c -b -f -l -r -v "${translation}" ${book} ${chapter}) # This calls the 'bg2md_mod script'
-  elif ${boldwords} -eq "true" && ${headers} -eq "true"; then
-    text=$(ruby BibleGateway-to-Markdown/bg2md.rb -c -b -f -l -r -v "${translation}" ${book} ${chapter}) # This calls the 'bg2md_mod script'
-  elif ${boldwords} -eq "false" && ${headers} -eq "true"; then
-    text=$(ruby BibleGateway-to-Markdown/bg2md.rb -e -c -f -l -r -v "${translation}" ${book} ${chapter}) # This calls the 'bg2md_mod script'
-  else
-    text=$(ruby BibleGateway-to-Markdown/bg2md.rb -e -c -f -l -r -v "${translation}" ${book} ${chapter}) # This calls the 'bg2md_mod script'
-  fi
+    # Choose the appropriate options for the conversion script based on `boldwords` and `headers`
+    if ${boldwords} == "true" && ${headers} == "false"; then
+      text=$(ruby BibleGateway-to-Markdown/bg2md.rb -e -c -b -f -l -r -v "${translation}" ${book} ${chapter})
+    elif ${boldwords} == "true" && ${headers} == "true"; then
+      text=$(ruby BibleGateway-to-Markdown/bg2md.rb -c -b -f -l -r -v "${translation}" ${book} ${chapter})
+    elif ${boldwords} == "false" && ${headers} == "true"; then
+      text=$(ruby BibleGateway-to-Markdown/bg2md.rb -e -c -f -l -r -v "${translation}" ${book} ${chapter})
+    else
+      text=$(ruby BibleGateway-to-Markdown/bg2md.rb -e -c -f -l -r -v "${translation}" ${book} ${chapter})
+    fi
 
     # Remove unwanted headers from the text
     text=$(echo $text | sed 's/^(.*?)v1/v1/') 
@@ -80,34 +83,31 @@ filename=${export_prefix}$export_number # Setting the filename
     # Add line breaks
     text=$(echo "$text" | awk '{gsub("v[0-9]","\\n\\n&")};1')
 
-  # Formatting the title for markdown
-  title="# ${book} ${chapter}"
+    # Formatting the title for markdown
+    title="# ${book} ${chapter}"
 
-  # Navigation format
-  export="${title}\n\n$navigation\n***\n\n$text\n\n***\n$navigation"
+    # Navigation format
+    export="${title}\n\n$navigation\n***\n\n$text\n\n***\n$navigation"
 
+    # Export
+    echo -e $text >> "$filename.md"
 
-  # Export
-  echo -e $text >> "$filename.md"
+    # Creating a folder
+    ((actual_num=book_counter+1)) # Proper number counting for the folder
 
-  # Creating a folder
+    if (( $actual_num < 10 )); then
+      #statements
+      actual_num="${actual_num}"
+    else
+      actual_num=$actual_num
+    fi
 
-  ((actual_num=book_counter+1)) # Proper number counting for the folder
+    folder_name="${book}" # Setting the folder name
 
-  if (( $actual_num < 10 )); then
-    #statements
-    actual_num="${actual_num}"
-  else
-    actual_num=$actual_num
-  fi
-
-  folder_name="${book}" # Setting the folder name
-
-  # Creating a folder for the book of the Bible it not existing, otherwise moving new file into existing folder
-  mkdir -p "./${translation}/${folder_name}"; mv "${filename}".md "${translation}/${folder_name}"
-
-
-done # End of the book exporting loop
+    # Creating a folder for the book of the Bible it not existing, otherwise moving new file into existing folder
+    mkdir -p "./${translation}/${folder_name}"; mv "${filename}".md "${translation}/${folder_name}"
+  
+  done # End of the book exporting loop
 
   # Create an overview file for each book of the Bible:
   overview_file="links: [[The Bible]]\n# ${book}\n\n[[${book} 1|Start Reading →]]"
@@ -115,7 +115,7 @@ done # End of the book exporting loop
   #mkdir -p ./Scripture ("${translation}")/"${folder_name}"; mv "$book.md" './Scripture ('"${translation}"')/'"${folder_name}"
   mv "$book.md" "${translation}/${folder_name}"
 
-  done
+done
 
   #----------------------------------------------------------------------------------
   # The Output of this text needs to be formatted slightly to fit with use in Obsidian
